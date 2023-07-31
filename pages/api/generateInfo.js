19282780713455746d74bc7828533911700ae6aa
JJ Behrens <jjinux@gmail.com>
2023-07-31 15:50:18 -0700

    feat: got the core OpenAI functionality working

---
+++ pages/api/generateInfo.js
@@ -0,0 +1,37 @@
+const { Configuration, OpenAIApi } = require("openai");
+const { recipePrompt } = require('../../data/recipe.json');
+
+const config = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(config);
+
+const generateInfo = async (req, res) => {
+    const { recipe } = req.body;
+    try {
+        const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: `${recipePrompt}${recipe}` }],
+            max_tokens: 200,
+            temperature: 0,
+            n: 1,
+        });
+        const response = completion.data.choices[0].message.content;
+        return res.status(200).json({
+            success: true,
+            data: response,
+        });
+    } catch (error) {
+        console.log(error);
+        if (error.response.status === 401) {
+            return res.status(500).json({
+                error: "OPENAI_API_KEY either wasn't set or isn't valid.",
+            });
+        }
+        return res.status(500).json({
+            error: "An error occurred while generating the recipe information. Please try again later.",
+        });
+    }
+};
+
+module.exports = { generateInfo };
