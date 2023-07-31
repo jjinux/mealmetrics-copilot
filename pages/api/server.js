6a4817963342f35424fb916be984ff199797cf25
JJ Behrens <jjinux@gmail.com>
2023-07-31 16:46:26 -0700

    chore: configured CORS

--- pages/api/server.js
+++ pages/api/server.js
@@ -3,10 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./router');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/openai', router);
 
