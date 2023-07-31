679eb6e342619143bc3032ca0d10f2cf6ddc8eb6
JJ Behrens <jjinux@gmail.com>
2023-07-31 16:45:59 -0700

    chore: configured CORS

--- pages/api/router.js
+++ pages/api/router.js
@@ -3,5 +3,11 @@ const { generateInfo } = require('./generateInfo');
 
 const router = express.Router();
 router.post('/generateInfo', generateInfo);
+router.options("/generateInfo", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.sendStatus(200);
+});
 
 module.exports = router;
