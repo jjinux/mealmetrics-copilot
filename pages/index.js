bea9f22bd3bb81424eec1934c9e62b76b3a1972f
JJ Behrens <jjinux@gmail.com>
2023-07-31 18:05:19 -0700

    feat: integrated all the other components

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,127 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import {
+    Button,
+    CircularProgress,
+    Container,
+    Grid,
+    Paper,
+    TextareaAutosize,
+    Typography,
+} from "@material-ui/core";
+import { Send } from "@material-ui/icons";
+import { ThemeProvider } from "@material-ui/core/styles";
 
-const RecipeInfo = () => {
-    const [recipe, setRecipe] = useState('');
-    const [nutrition, setNutrition] = useState('');
+import Header from "../components/Header";
+import NutritionFacts from "../components/NutritionFacts";
+import Footer from "../components/Footer";
+import theme from "../utils/theme";
 
-    const handleSubmit = async () => {
+function HomePage() {
+    const [recipe, setRecipe] = useState("");
+    const [nutrition, setNutrition] = useState("");
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        setLoading(true);
         try {
-            const recipeInfo = await axios.post('http://localhost:8080/openai/generateinfo', { recipe });
-            setNutrition(recipeInfo.data.data);
+            const response = await fetch(
+                "http://localhost:8080/openai/generateinfo",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ recipe }),
+                }
+            );
+
+            // Get the nutrition info.
+            const nutrition = await response.json();
+
+            // Display the nutrition info.
+            if (nutrition.data) {
+                setNutrition(nutrition.data);
+            } else {
+                setError("Unable to get nutrition info.");
+            }
+            setLoading(false);
         } catch (error) {
-            console.log(error);
+            setError(error);
+            setLoading(false);
         }
-    };
+    }
+
+    function handleClear(event) {
+        event.preventDefault();
+        setRecipe("");
+    }
 
     return (
-        <div>
-            <h1>Find nutrition facts for any recipe</h1>
-            <textarea
-                value={recipe}
-                onChange={(e) => setRecipe(e.target.value)}
-                placeholder="Enter your recipe here..."
-            />
-            <button onClick={handleSubmit}>Submit</button>
-            <div>
-                <h2>Nutrition facts</h2>
-                <p>{nutrition}</p>
-            </div>
-        </div>
+        <ThemeProvider theme={theme}>
+            <Header />
+            <Container maxWidth="md" style={{ marginTop: "40px", minHeight: "100vh", paddingBottom: "100px" }}>
+                <Typography variant="h3" gutterBottom>
+                    🍎 Find nutrition facts for any recipe
+                </Typography>
+                <Paper elevation={24} style={{ padding: "20px" }}>
+                    <form onSubmit={handleSubmit}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextareaAutosize
+                                    value={recipe}
+                                    onChange={(e) => setRecipe(e.target.value)}
+                                    placeholder="Enter recipe to get nutrition facts"
+                                    style={{
+                                        width: "98%",
+                                        maxWidth: "850px",
+                                        minHeight: "200px",
+                                        padding: "10px",
+                                    }}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Button
+                                    type="submit"
+                                    variant="contained"
+                                    color="primary"
+                                    startIcon={<Send />}
+                                >
+                                    Submit
+                                </Button>
+                                <br />
+                                <Button
+                                    style={{ marginTop: "10px" }}
+                                    variant="contained"
+                                    color="secondary"
+                                    onClick={handleClear}
+                                >
+                                    Clear
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </Paper>
+
+                <div style={{ paddingTop: "40px" }}>
+                    {loading && (
+                        <Grid item xs={12}>
+                            <CircularProgress />
+                        </Grid>
+                    )}
+                    {error ? (
+                        <Typography color="error">
+                            An error occurred: {error.errorMessage}
+                        </Typography>
+                    ) : nutrition ? (
+                        <NutritionFacts data={nutrition} />
+                    ) : null}
+                </div>
+            </Container>
+            <Footer />
+        </ThemeProvider>
     );
 }
 
-export default RecipeInfo;
+export default HomePage;
